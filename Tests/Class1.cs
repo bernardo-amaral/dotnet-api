6a8ff622da326341a3932ec9434c806825739b62
Bernardo Amaral <b.aoliveira@tjrs.jus.br>
2019-02-12 13:17:57 -0200

    Tests interface with xunit

---
+++ Tests/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
